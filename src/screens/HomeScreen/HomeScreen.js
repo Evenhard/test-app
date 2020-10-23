4e6f161a270c007530cfe1913089e4e453fb9211
Kirill <sku@sionic.ru>
2020-10-23 15:31:17 +0700

    api v2 test

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import {GenericTemplate} from '../../components';
+import {Button, GenericTemplate} from '../../components';
 import CategoriesContainer from './containers/CategoriesContainer';
+// import {DefaultApi, ApiClient} from '../../api';
+// import DefaultApi from '../../api/api/DefaultApi';
 
 const HomeScreen = () => {
-    return (
-        <GenericTemplate scrollable={false}>
-            <CategoriesContainer/>
-        </GenericTemplate>
-    );
+  const OpenApiCitypizza = require('../../api/index');
+  const api = new OpenApiCitypizza.DefaultApi();
+  // const category = new OpenApiCitypizza.Category(); // {Client} client model
+  const callback = function (error, data, response) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('API called successfully. Returned data: ' + data);
+    }
+  };
+
+  const foo = () => {
+    const category = api.categoryBySlug('sushi', callback);
+    console.log(category.data);
+
+    // api.ping(1, callback);
+
+    // const SVC = new OpenApiCitypizza.DefaultApi();
+    // const MODEL = new OpenApiCitypizza.Banner();
+    //
+    // const category = SVC.categoryBySlug('sushi');
+    // console.log(category);
+    // const category = DefaultApi.categoryBySlug('sushi');
+    // console.log(category);
+  };
+
+  return (
+    <GenericTemplate scrollable={false}>
+      <Button onPress={foo} title={'API'} />
+      <CategoriesContainer />
+    </GenericTemplate>
+  );
 };
 
 export default HomeScreen;
